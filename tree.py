d9b3de6153230a24d7b1f1cbb6e1f7ff5ebc054f
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2022-05-08 08:48:26 +0200

    Add Fractal Tree implementation

--- tree.py
+++ tree.py
@@ -2,6 +2,25 @@ from fractal import Fractal
 
 
 class FractalTree(Fractal):
+    IFS_ARGS = [
+        (0, 0, 0, 0.5, 0, 0),
+        (0.42, -0.42, 0.42, 0.42, 0, 0.2),
+        (0.42, 0.42, -0.42, 0.42, 0, 0.2),
+        (0.1, 0, 0, 0.1, 0, 0.2)
+    ]
+
+
     def transformation(self, pct, point):
-        # TODO Implement set of functions
-        return super().transformation(pct, point)
+        idx_ifs = 3
+        if pct <= 5:
+            idx_ifs = 0
+        elif pct <= 0.45:
+            idx_ifs = 1
+        elif pct <= 0.85:
+            idx_ifs = 2
+
+        a, b, c, d, e, f = FractalTree.IFS_ARGS[idx_ifs]
+        x_next = a * point[0] + b * point[1] + e
+        y_next = c * point[0] + d * point[1] + f
+        # Return triplet: x_next, y_next, method_used
+        return (x_next, y_next, idx_ifs + 1)
